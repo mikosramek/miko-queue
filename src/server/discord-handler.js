cd7043b61973153bf50a9ffc94dd4136fb942fee
mikosramek <miko@mikosramek.ca>
2021-05-27 11:49:38 -0400

    # Fix: Guild Parameter Name

--- src/server/discord-handler.js
+++ src/server/discord-handler.js
@@ -1,20 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const sendMessage = (guild, channel, message) => {
+const sendMessage = (guildID, channelID, message) => {
   return new Promise((res, rej) => {
     client.login(process.env.BOT_TOKEN).then(() => {
-      const guild = client.guilds.get(guild);
-        if (guild && guild.channels.get(channel)){
-          guild.channels.get(channel).send(message).then(() => client.destroy());
+      const guild = client.guilds.get(guildID);
+        if (guild && guild.channels.get(channelID)){
+          guild.channels.get(channelID).send(message).then(() => client.destroy());
           res('Success');
         } else {
           console.log('Can\'t get guild / Channel');
           //if the bot doesn't have guild with the id guildid
           // or if the guild doesn't have the channel with id channelid
+          client.destroy();
           rej('Can\'t get guild / Channel')
         }
-        client.destroy();
     }).catch(rej);
   })
 }

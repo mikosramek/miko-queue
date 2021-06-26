f3cdd4b99602af566be6abccef9d6146517b0175
mikosramek <miko@mikosramek.ca>
2021-06-26 02:08:19 -0400

    Feature: Adding pending webhook status

--- src/server/route/webhooks.js
+++ src/server/route/webhooks.js
@@ -7,6 +7,9 @@ const DiscordUtil = require('../util/discord-handler');
 const GUILD_ID = process.env.GUILD_ID;
 const CHANNEL_ID = process.env.DEBUG_CHANNEL_ID;
 
+let sentPendingNotif = false;
+let sentSuccessNotif = false;
+let sendOtherNotif = false;
 
 const customParser = bodyParser.json({type: function(req) {
     if (req.headers['content-type'] === ""){
@@ -36,17 +39,30 @@ router.post('/heroku', customParser, async (req, res) => {
     console.info(data);
     const name = _get(data, 'app.name', '-');
     const status = _get(data, 'status', 'webhook error');
+    const output = _get(data, 'output_stream_url', '-');
     const release = _get(data, 'release.version', null);
     const commit = _get(data, 'slug.commit', '-');
     const commitDesc = _get(data, 'slug.commit_description', '-');
 
-    if (release) {
-      const cardEmbed = new Embed(`${name} update`, '#ffffff', null, null);
+    console.info({ name, status, release, commit, commitDesc }, 'webhooks.js@');
+
+    if (status === 'succeeded' && !sentSuccessNotif) {
+      sentSuccessNotif = true;
+      const cardEmbed = new Embed(`${name} update`, '#ffffff', output, null);
       cardEmbed.addField('Status:', status);
       cardEmbed.addField('Version:', release, true);
       cardEmbed.addField('Commit Hash:', commit, true);
-      cardEmbed.addField('Update:', commitDesc);
+      cardEmbed.addField('Update:', commitDesc || '-');
       await DiscordUtil.sendMessage(GUILD_ID, CHANNEL_ID, cardEmbed.embed);
+    }
+    else if (status === 'pending' && !sentPendingNotif) {
+      sentPendingNotif = true;
+      const cardEmbed = new Embed(`${name} update`, '#ffffff', output, null);
+      cardEmbed.addField('Status:', status);
+      await DiscordUtil.sendMessage(GUILD_ID, CHANNEL_ID, cardEmbed.embed);
+    }
+    else if (!sendOtherNotif) {
+      sendOtherNotif = true;
     }
 
     res.status(204).send();
